0a8bef068095f6a06a62129036471d8de5cd48af
Ryan McCall <ryan.e.mccall@gmail.com>
2020-02-18 17:03:50 -0600

    Start 4.3 exercises

---
+++ 4.3_data_types_operators_and_variables.py
@@ -0,0 +1,76 @@
+# Identify the data type of the following values:
+    # 99.9
+        # float
+    # "False"
+        # string
+    # False
+        # boolean
+    # '0' 
+        # string
+    # 0
+        # integer
+    # True
+        # boolean
+    # 'True'
+        # string
+    # [{}]
+        # list
+    # {'a': []}
+        # dictionary
+
+# What data type would best represent:
+    # A term or phrase typed into a search box?
+        # string
+    # If a user is logged in?
+        # boolean
+    # A discount amount to apply to a user's shopping cart?
+        # integer or float
+    # Whether or not a coupon code is valid?
+        # boolean
+    # An email address typed into a registration form?
+        # string
+    # The price of a product?
+        # float
+    # A Matrix?
+        # list
+    # The email addresses collected from a registration form?
+        # string
+    # Information about applicants to Codeup's data science program?
+        # dict, list, or string
+
+# For each of the following code blocks, read the expression and predict what the result of
+# evaluating it would be, then execute the expression in your Python REPL.
+    # '1' + 2
+        # Error
+    # 6 % 4
+        # 2
+    # type(6 % 4)
+        # int
+    # type(type(6 % 4))
+        # type
+    # '3 + 4 is ' + 3 + 4
+        # Error
+    # 0 < 0
+        # False
+    # 'False' == False
+        # False
+    # True == 'True'
+        # False
+    # 5 >= -5
+        # True
+    # !False or False
+        
+    # True or "42"
+    # !True && !False
+    # 6 % 5
+    # 5 < 4 and 1 == 1
+    # 'codeup' == 'codeup' and 'codeup' == 'Codeup'
+    # 4 >= 0 and 1 !== '1'
+    # 6 % 3 == 0
+    # 5 % 2 != 0
+    # [1] + 2
+    # [1] + [2]
+    # [1] * 2
+    # [1] * [2]
+    # [] + [] == []
+    # {} + {}
