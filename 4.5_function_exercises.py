86b194023b2882a9be441882808ef354599bcb7c
Ryan McCall <ryan.e.mccall@gmail.com>
2020-02-22 17:50:07 -0600

    Finish regular problems for 4.5

--- 4.5_function_exercises.py
+++ 4.5_function_exercises.py
@@ -58,17 +58,104 @@ print(is_consonant(3))
 
 # Define a function that accepts a string that is a word. The function should capitalize 
 # the first letter of the word if the word starts with a consonant.
+def capital(word):
+    if is_consonant(word[0]):
+        return word.title()
+    return word
 
+print(capital("apple"))
+print(capital("banana"))
+print(capital("carrot"))
+print(capital("danish"))
+print(capital("eggplant"))
 
+# Define a function named calculate_tip. It should accept a tip percentage (a number 
+# between 0 and 1) and the bill total, and return the amount to tip.
+def calculate_tip(tip_percentage, total):
+    return tip_percentage * total
 
+print(calculate_tip(.20, 10))
 
+# Define a function named apply_discount. It should accept a original price, and a 
+# discount percentage, and return the price after the discount is applied.
+def apply_discount(discount_percentage, total):
+    return total - (discount_percentage * total)
 
+print(apply_discount(.1, 10))
 
+# Define a function named handle_commas. It should accept a string that is a number that 
+# contains commas in it as input, and return a number as output.
+def handle_commas(string_number):
+    return int(string_number.replace(",", ""))
 
+print(handle_commas("1,000"))
+print(handle_commas("1,000,000"))
+print(handle_commas("1,000,000,000"))
+
+# Define a function named get_letter_grade. It should accept a number and return the 
+# letter grade associated with that number (A-F).
+def get_letter_grade(number_grade):
+    if number_grade >= 90:
+        return "A"
+    elif number_grade >= 80:
+        return "B"
+    elif number_grade >= 70:
+        return "C"
+    elif number_grade >= 60:
+        return "D"
+    else:
+        return "F"
+
+# Define a function named remove_vowels that accepts a string and returns a string with 
+# all the vowels removed.
+def remove_vowels(string):
+    string_without_vowels = []
+
+    for character in string:
+        if character.lower() not in 'aeiou':
+            string_without_vowels.append(character)
+
+    return ''.join(string_without_vowels)
+
+print(remove_vowels("banana"))
+print(remove_vowels("calamatorium"))
+
+# Define a function named normalize_name. It should accept a string and return a valid 
+# python identifier, that is:
+# anything that is not a valid python identifier should be removed
+# leading and trailing whitespace should be removed
+# everything should be lowercase
+# spaces should be replaced with underscores
+# for example:
+# Name will become name
+# First Name will become first_name
+# % Completed will become completed
+import re
+def normalize_name(string):
+    string = re.sub('[^A-Za-z0-9 _]+', '', string)
+
+    while string[0].isdigit():
+        string = string[1:len(string)]
+
+    string = string.strip()
+    string = string.replace(" ", "_")
+    string = string.lower()
+    return string
+
+print(normalize_name("Name"))
+print(normalize_name("First Name"))
+print(normalize_name("% Completed"))
+
+# Write a function named cumsum that accepts a list of numbers and returns a list that is 
+# the cumulative sum of the numbers in the list.
 def cumsum(num_list):
     sum_list = []
     sum_stor = 0
     for num in num_list:
-        sum_list.append(sum_stor + sum_list)
+        sum_list.append(sum_stor + num)
+        sum_stor += num
     return sum_list
-        
+
+print(cumsum([1, 2, 3]))
+print(cumsum([1, 1, 1]))
+print(cumsum([1, 2, 3, 4]))
